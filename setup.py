d1140f8f6cf1558a0ea164aa2a43a2276e0ff2b5
Olga Bulat <obulat@gmail.com>
2020-05-22 16:20:57 +0300

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/obulat/trLemmer',
-    version='0.1.0',
+    version='0.1.1-alpha',
     zip_safe=False,
 )
