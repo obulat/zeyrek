bf5963ffbcd06cd07c9cf35a3c6d863083aa1b21
Olga Bulat <obulat@gmail.com>
2020-12-30 21:32:39 +0300

    Make examples executable and add nltk download

--- examples/readme_examples.py
+++ examples/readme_examples.py
@@ -1,13 +1,22 @@
 from zeyrek import MorphAnalyzer
-analyzer = MorphAnalyzer()
+import nltk
+nltk.download('punkt')
 
-with open('text.txt', encoding='utf-8') as text_file:
-    text = text_file.read()
 
-# Analyze text from the file and print out formatted parses of each word.
-result = analyzer.analyze(text)
-for word_result in result:
-    print(word_result[0].word)
-    for parse in word_result:
-        print(parse.formatted)
-    print()
+def run_examples():
+    analyzer = MorphAnalyzer()
+
+    with open('text.txt', encoding='utf-8') as text_file:
+        text = text_file.read()
+
+    # Analyze text from the file and print out formatted parses of each word.
+    result = analyzer.analyze(text)
+    for word_result in result:
+        print(word_result[0].word)
+        for parse in word_result:
+            print(parse.formatted)
+        print()
+
+
+if __name__ == '__main__':
+    run_examples()
