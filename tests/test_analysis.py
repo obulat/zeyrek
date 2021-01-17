5238b2712540cc1d7a035526741ed7ca46ca7d46
Olga Bulat <obulat@gmail.com>
2021-01-17 16:44:42 +0300

    Fix tests and requırements
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -5,20 +5,11 @@
 
 import pytest
 
-from zeyrek.attributes import SecondaryPos, PrimaryPos, calculate_phonetic_attributes
+from zeyrek.attributes import SecondaryPos, PrimaryPos, calculate_phonetic_attributes, RootAttribute, PhoneticAttribute
 from zeyrek.lexicon import DictionaryItem, RootLexicon
 from zeyrek.morphology import MorphAnalyzer
-from zeyrek.morphotactics import StemTransition, SearchPath, root_S
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from zeyrek.morphotactics import StemTransition, SearchPath, root_S, noun_S, MorphemeState, p2sg_S, loc_ST, a3pl_S, \
+    SuffixTransition, morphemes, equ_ST, TurkishMorphotactics, adjectiveRoot_ST
 
 
 @pytest.fixture
@@ -43,7 +34,6 @@ def test_rootlexicon(dict_item):
     lex.add(dict_item)
     assert dict_item in lex.item_set
     assert dict_item.id_ in lex.id_dict
-    print(dict_item.id_)
     item = lex.get_item_by_id(dict_item.id_)
     assert item == dict_item
     assert len(lex) == 1
@@ -60,48 +50,23 @@ def test_initial_search_path():
     assert p.stem_transition == transition
 
 
-"""
 def test_stem_transition():
     from zeyrek.attributes import calculate_phonetic_attributes
-    word = 'beyaz'
-    attr = DictItemAttrs(RootAttribute(0), calculate_phonetic_attributes(word))
-    dict_item = DictionaryItem(word, word, PrimaryPos.Adjective,
-                               SecondaryPos.NONE, attr, word, 0)
-    transition = StemTransition(word, dict_item,
-                                attr, noun_S)
-    assert transition.to_ == noun_S
-    # assert str(transition) == "<(Dict: beyaz [P:Adj]):beyaz → [noun_S:Noun]>"
-    print(transition, dir(transition))
+    word_line = 'beyaz [P:Adj]'
+    lexicon = RootLexicon.from_lines([word_line])
+    morphotactics = TurkishMorphotactics(lexicon=lexicon)
+    dict_item = lexicon.get_matching_items('beyaz')[0]
+    transition = morphotactics.stem_transitions.prefix_matches('beyaz')[0]
+    assert transition.to_ == adjectiveRoot_ST
+    assert str(transition) == "<(Dict: beyaz [P:Adj]):beyaz → [adjectiveRoot_ST:Adj]>"
     assert transition.condition is None
     assert transition.condition_count == 0
     assert transition.dict_item.lemma == 'beyaz'
-    assert transition.from_ is None
-    print(transition.phonetic_attrs)
+    assert transition.from_ is root_S
 
-    assert transition.phonetic_attrs & (PhoneticAttribute.FirstLetterConsonant | PhoneticAttribute.LastLetterConsonant)
-    assert transition.surface == dict_item.lemma
+    calculated_attrs = calculate_phonetic_attributes('beyaz')
+    assert transition.attrs == calculated_attrs
     assert type(transition.to_) == MorphemeState
-    assert transition.to_ == noun_S
-
-
-
-def test_morphotactics():
-    item = DictionaryItem("guzel", "guzel", PrimaryPos.Adjective, SecondaryPos.NONE, [], "guzel", 0)
-    lex = RootLexicon()
-    lex.add(item)
-    m = TurkishMorphotactics(lex)
-    print(m)
-    assert m is not None
-
-    from zeyrek.conditions import PreviousMorphemeIs
-    trans = SuffixTransition(p2sg_S, equ_ST, 'cA', PreviousMorphemeIs(morphemes['A3pl']))
-    print(trans, trans.condition, trans.condition_count)
-    pre_trans = SuffixTransition(a3pl_S, p2sg_S)
-
-    assert trans is not None
-    path = SearchPath('ca', MorphemeState(loc_ST, loc_ST, True), [a3pl_S, p2sg_S], PhoneticAttribute.LastLetterVowel, True)
-    assert trans.condition.accept(path)
-"""
 
 
 @pytest.fixture
@@ -112,10 +77,8 @@ def lex_from_lines():
 def test_analysis(lex_from_lines):
     lemmer = MorphAnalyzer(lexicon=lex_from_lines)
     analysis = lemmer.analyze('elma')
-    print(analysis)
     assert analysis is not None
     analysis = lemmer.analyze('beyazlaştırıcı')
-    print(analysis)
     assert analysis is not None
     assert 'beyaz' in lemmer.lemmatize('beyazlaştı')[0][1]
     assert 'elma' in lemmer.lemmatize('elmalı')[0][1]
